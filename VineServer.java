6b8f089ca2351e80ace9e5b1817123260dc16026
tutecs <elduryea@gmail.com>
2018-04-19 21:36:27 -0400

    Added a method to send the scores of each client the client our thread is currently working for.

--- VineServer.java
+++ VineServer.java
@@ -124,10 +124,12 @@ class Client {
 	private int score = 0;
 	private InetAddress address;
 	private int port;
-	public Client(InetAddress address, int port, int id) {
+	private String name;
+	public Client(InetAddress address, int port, int id, String name) {
 		this.address = address;
 		this.port = port;
 		this.id = id;
+		this.name = name;
 	}
 	// Getter methods for score, address and port
 	public int getScore() {
@@ -142,6 +144,9 @@ class Client {
 	public int getID() {
 		return id;
 	}
+	public String getName() {
+		return name;
+	}
 	// Score increment method
 	public void addScore(int value) {
 		score += value;
@@ -150,13 +155,19 @@ class Client {
 
 // Thread class for each connected client
 class ClientServer implements Runnable {
-	private Thread t;
+	// Static HashMap will be shared by every thread.
+	// New user:scores pairs will be updated by the main loop
+	// the scores will be updated by the threads
+	private static Map<String,Integer> scores = new ConcurrentHashMap<String, Integer>();
+
 	// Static Quiz that will be updated by our main loop and sent to the client
 	private static Quiz currentQuiz = null;
 	private static int currentID = 0;
+	private Thread t;
 	private String threadName;
 	private Client client;
 	private Socket socket;
+	private volatile boolean active;
 	public ClientServer(Socket connection) {
 		InetAddress address = connection.getInetAddress();
 		int port = connection.getPort();
@@ -164,6 +175,7 @@ class ClientServer implements Runnable {
 		this.threadName = String.valueOf(id);
 		this.client = new Client(address, port , id);
 		this.socket = connection;
+		this.active = true;
 	}
 	// The run method will send the quiz info to the client.
 	// We will also receive which answer the client choose, determine if the answer was correct, and give
@@ -174,8 +186,12 @@ class ClientServer implements Runnable {
 			Quiz prevQuiz = null;
 			// We should probably add some kind of exit to this loop
 			// Like when the client disconnects
-			while(true) {
+			while(active) {
 				String request = inFromClient.readLine();
+				if(request == null) {
+					active = false;
+					break;
+				}
 				String[] requestWords = request.split(" ");
 				if(requestWords.length == 3) {
 					if(requestWords[0].equals("GET") && requestWords[1].equals("/")) {
@@ -191,6 +207,12 @@ class ClientServer implements Runnable {
 					// Need to get variable string answer from client
 					score = currentQuiz.calcScore(answer);
 					client.addScore(score);
+					// Update the scores map
+					scores.put(client.getName(), client.getScore());
+
+					// Send the scores of each client to this client
+					sendScores();
+
 					prevQuiz = currentQuiz;
 				}
 			}
@@ -204,64 +226,90 @@ class ClientServer implements Runnable {
 			t.start();
 		}
 	}
-
 	// The sendQuiz function and the waitAndReceive function have redundant code. Can we fix this?
 
 	// Send the quiz in json format.
 	// Wait for a GET request
 	public void sendQuiz() {
-		while(true) {
-			String request = inFromClient.readLine();
-			String[] requestWords = request.split(" ");
-			if(requestWords.length == 3) {
-				if(requestWords[0].equals("GET") && requestWords[1].equals("/")) {
-					String page = getPage("index.html");
-					outToClient.writeBytes(page);
-				}
-				if(requestWords[0].equals("GET") && requestWords[1].equals("/quiz")) {
-					String[] answers = currentQuiz.getAnswers();
-					String imagePath = currentQuiz.getImagePath();
-					String videoPath = currentQuiz.getVideoPath();
-					String json = String.format("\"item1\":\"%s\", \"item2\":\"%s\", \"item3\":\"%s\", \"item4\":\"%s\", \"imgPath\":\"%s\", \"vidPath\":\"%s\"",
-						answer[0], answer[1], answer[2], answer[3], imagePath, videoPath);
+		try {
+			while(true) {
+				String request = inFromClient.readLine();
+				String[] requestWords = request.split(" ");
+				if(requestWords.length == 3) {
+					if(requestWords[0].equals("GET") && requestWords[1].equals("/")) {
+						String page = getPage("index.html");
+						outToClient.writeBytes(page);
+					}
+					if(requestWords[0].equals("GET") && requestWords[1].equals("/quiz")) {
+						String[] answers = currentQuiz.getAnswers();
+						String imagePath = currentQuiz.getImagePath();
+						String videoPath = currentQuiz.getVideoPath();
+						String json = String.format("\"item1\":\"%s\", \"item2\":\"%s\", \"item3\":\"%s\", \"item4\":\"%s\", \"imgPath\":\"%s\", \"vidPath\":\"%s\"",
+							answer[0], answer[1], answer[2], answer[3], imagePath, videoPath);
 
-					outToClient.writeBytes(json);
-					return;
+						outToClient.writeBytes(json);
+						return;
+					}
 				}
 			}
+		} catch (IOException e) {
+			active = false;
+			e.printTraceStack();
 		}
 	}
 	// Wait to receive the client's answer to the Quiz. While we wait, check for page requests, ya know.
 	public int waitAndReceive(BufferedReader inFromClient) {
-		while(true) {
-			String request = inFromClient.readLine();
-			String[] requestWords = request.split(" ");
-			if(requestWords.length == 3) {
-				if(requestWords[0].equals("GET") && requestWords[1].equals("/")) {
-					String page = getPage("index.html");
-					outToClient.writeBytes(page);
-				}
-				if(requestWords[0].equals("POST") && requestWords[1].equals("/submit")) {
-					// Code for parsing the POST request
-					// Should be in x-www-form-urlencoded format
-					boolean noBlank = true;
-					String line = "";
-					int contentLength = 0;
-					while(noBlank) {
-						line = inFromClient.readLine();
-						if(s.toLowerCase().contains("Content-length"))
-							contentLength = Integer.valueOf(s.split[1]);
-						if(s.equals(""))
-							noBlank = false;
+		try {
+			while(true) {
+				String request = inFromClient.readLine();
+				String[] requestWords = request.split(" ");
+				if(requestWords.length == 3) {
+					if(requestWords[0].equals("GET") && requestWords[1].equals("/")) {
+						String page = getPage("index.html");
+						outToClient.writeBytes(page);
 					}
-					char[] body = new char[contentLength];
-					String body = String.valueOf(body);
-					int answer = Integer.valueOf(body.split("=")[1]);
-					return answer;
+					if(requestWords[0].equals("POST") && requestWords[1].equals("/submit")) {
+						// Code for parsing the POST request
+						// Should be in x-www-form-urlencoded format
+						boolean noBlank = true;
+						String line = "";
+						int contentLength = 0;
+						while(noBlank) {
+							line = inFromClient.readLine();
+							if(s.toLowerCase().contains("Content-length"))
+								contentLength = Integer.valueOf(s.split[1]);
+							if(s.equals(""))
+								noBlank = false;
+						}
+						char[] body = new char[contentLength];
+						String body = String.valueOf(body);
+						int answer = Integer.valueOf(body.split("=")[1]);
+						return answer;
+					}
 				}
 			}
+		} catch (IOException e) {
+			active = false;
+			e.printTraceStack();
 		}
 	}
+	public void sendScores() {
+		int nClients = scores.size();
+		String json = String.format("{\"clients\":\"%d\"", nClients)
+		for(Map.Entry<String,Integer> entry : scores.entrySet()) {
+			String username = entry.getKey();
+			int score = entry.getValue();
+			json = String.format("%s,\"%s\":\"%d\"", json, username, score);
+		}
+		json = json + "}";
+		try {
+			outToClient.writeBytes(json);
+		} catch (IOException e) {
+			e.printTraceStack();
+			active = false;
+		}
+	}
+
 	// Get the html code for a given page name
 	// returns a single string with all the html code
 	public String getPage(String pageName) {
@@ -286,6 +334,7 @@ class ClientServer implements Runnable {
 			return headerBody;
 		}
 	}
+	// get the html code from the file
 	private static String getHTML(String filename)
 	{
 		try
