84720687655aba5a2e333c711f6d780b4a6ad4a8
tutecs <elduryea@gmail.com>
2018-05-15 14:42:53 -0400

    Server is now functional

--- VineServer.java
+++ VineServer.java
@@ -26,6 +26,7 @@ public class VineServer {
 			ServerSocket listenSocket = new ServerSocket(port);
 			// This represents the previous number connected clients.
 			// This variable will be used to trigger the quizMaker thread.
+			Socket connectionSocket = null;
 			int prevN = 0;
 			while(true) {
 				for(int i = 0; i < clients.size(); i++) {
@@ -33,17 +34,22 @@ public class VineServer {
 						clients.remove(i);
 					}
 				}
-				Socket connectionSocket = listenSocket.accept();
+				connectionSocket = listenSocket.accept();
+				System.out.println(listenSocket.getInetAddress().getCanonicalHostName());
 				ClientServer server = new ClientServer(connectionSocket);
 				server.start();
 				clients.add(server);
+				System.out.printf("Connected Clients: %d\n", clients.size());
 				if(clients.size() == 1 && clients.size() - prevN > 0) {
+					System.out.println("Starting QuizGetter Thread");
 					quizGetter = new QuizGetter("QuizGetter", 13);
 					quizGetter.start();
 				}
 				if(clients.size() == 0 && clients.size() - prevN < 0) {
+					System.out.println("Stopping QuizGetter Thread");
 					quizGetter.stop();
 				}
+				prevN = clients.size();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -62,23 +68,18 @@ class QuizGetter implements Runnable {
 		this.active = true;
 	}
 	public void run() {
-		// A date object to help schedule new quiz updates
-		Date date = new Date();
-		long beginTime = date.getTime();
-		while(active) {
-			// Get the current time
-			long currentTime = date.getTime();
-			// Get the difference in time
-			long diffInMillis = currentTime - beginTime;
-			// Conver the difference from millieseconds to seconds
-			long diffInSeconds = TimeUnit.SECONDS.convert(diffInMillis, TimeUnit.MILLISECONDS);
-			// Load a new quiz every 7 seconds
-			if(diffInSeconds >= updateTime) {
+		System.out.println("Making a new quiz");
+		Quiz quiz = Quiz.newQuiz();
+		ClientServer.updateQuiz(quiz);
+	 	Timer t = new Timer();
+		t.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println("Making a new quiz");
 				Quiz quiz = Quiz.newQuiz();
 				ClientServer.updateQuiz(quiz);
-				beginTime = date.getTime();
 			}
-		}
+		}, 0, updateTime*1000);
 	}
 	public void start() {
 		if(t == null) {
@@ -153,7 +154,7 @@ class Quiz {
 		for(int i = 1; i < randAns.length; i++) {
 			rand = random.nextInt(vines.size());
 			// make sure there are no repeating answers
-			while(rand == randX && rand == randAns[1] && rand == randAns[2] && rand == randAns[3]) {
+			while(rand == randX || rand == randAns[1] || rand == randAns[2] || rand == randAns[3]) {
 				rand = random.nextInt(vines.size());
 			}
 			randAns[i] = rand;
@@ -162,9 +163,9 @@ class Quiz {
 		int correctIdx = -1;
 		int[] answers = {-1,-1,-1,-1};
 		for(int i = 0; i < answers.length; i++) {
-			rand = random.nextInt(vines.size());
+			rand = random.nextInt(answers.length);
 			while(answers[rand] != -1) {
-				rand = random.nextInt(vines.size());
+				rand = random.nextInt(answers.length);
 			}
 			if(randAns[i] == randX) {
 				correctIdx = rand;
@@ -188,9 +189,10 @@ class Quiz {
 		try {
 			FileReader fileReader = new FileReader("vines.txt");
 			BufferedReader reader = new BufferedReader(fileReader);
-			String line = null;
+			String line = "";
 			while((line = reader.readLine()) != null) {
 				String[] elements = line.split(",");
+				System.out.printf("Elements size %d\n", elements.length);
 				String vineName = elements[0];
 				String[] paths = {elements[1], elements[2]};
 				vines.put(vineName, paths);
@@ -237,6 +239,9 @@ class Client {
 	public void addScore(int value) {
 		score += value;
 	}
+	public void setName(String username) {
+		this.name = username;
+	}
 }
 
 // Thread class for each connected client
@@ -248,6 +253,7 @@ class ClientServer implements Runnable {
 
 	// Static Quiz that will be updated by our main loop and sent to the client
 	private static volatile Quiz currentQuiz = null;
+	private static long newQuizTime = 0;
 	private static volatile int currentID = 0;
 	private Thread t;
 	private String threadName;
@@ -270,9 +276,7 @@ class ClientServer implements Runnable {
 			this.socket = connection;
 			this.inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			this.outToClient = new DataOutputStream(socket.getOutputStream());
-			String username = getUsername();
-			System.out.println(username);
-			this.client = new Client(address, port, id, username);
+			this.client = new Client(address, port, id, null);
 			this.state = 0;
 			this.active = true;
 		} catch (IOException e) {
@@ -286,27 +290,27 @@ class ClientServer implements Runnable {
 		System.out.println("Running thread");
 		try {
 			Quiz prevQuiz = null;
-
+			if(client.getName() == null) {
+				getUsername();
+			}
 			// run this loop until we detect that we are disconnected from the client
 			while(active) {
 				String request = inFromClient.readLine();
-				if(request == null) {
-					active = false;
-					break;
-				}
-				String[] requestWords = request.split(" ");
-				if(requestWords.length == 3) {
-					if(requestWords[0].equals("GET")) {
-						sendPage(requestWords[1]);
+				if(request != null) {
+					String[] requestWords = request.split(" ");
+					if(requestWords.length == 3) {
+						if(requestWords[0].equals("GET")) {
+							sendPage(requestWords[1]);
+						}
 					}
 				}
-				if(currentQuiz != prevQuiz) {
-					// Send the quiz info to the client using xmlhttprequest
-					sendQuiz();
-					waitAndReceive(inFromClient);
-
-					prevQuiz = currentQuiz;
-				}
+				// if(currentQuiz != prevQuiz) {
+				// 	// Send the quiz info to the client using xmlhttprequest
+				// 	sendQuiz();
+				// 	waitAndReceive(inFromClient);
+				//
+				// 	prevQuiz = currentQuiz;
+				// }
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
@@ -321,21 +325,30 @@ class ClientServer implements Runnable {
 	public boolean isActive() {
 		return active;
 	}
-	
+
 	// The sendQuiz function and the waitAndReceive function have redundant code. Can we fix this?
 	public void sendPage(String pageName) {
 		String page = "";
 		if(pageName.equals("/")) {
 			System.out.println("Serving vine website");
-			page = getPage("VineWebsite.html");
-		}
-		else if(pageName.equals("/")) {
 			page = getPage("vinequiz.html");
+		}
+		else if(pageName.equals("/quiz")) {
+			page = getPage("vinequiz.html");
+		}
+		else if(pageName.equals("/getQuiz")) {
+			sendQuiz();
+			return;
+		}
+		else if(pageName.contains("images")) {
+			sendImage(pageName);
+			return;
 		}
 		else {
 			page = getPage("not found");
 		}
 		try {
+			System.out.printf("Writing %s out to client\n", pageName);
 			outToClient.writeBytes(page);
 		} catch (IOException e) {
 			active = false;
@@ -344,31 +357,37 @@ class ClientServer implements Runnable {
 	}
 	// Send the quiz in json format.
 	// Wait for a GET request
-	public String getUsername() {
+	public void getUsername() {
 		System.out.println("Running getUsername");
 		try {
 			while(true) {
 				String request = inFromClient.readLine();
 				if(request != null) {
+					System.out.println(socket.getKeepAlive());
 					System.out.printf("GET INFO: %s\n", request);
 					String[] requestWords = request.split(" ");
 					if(requestWords.length == 3) {
 						if(requestWords[0].equals("GET")) {
 							System.out.printf("requested page from GET %s\n", requestWords[1]);
 							sendPage(requestWords[1]);
+							while(!(request = inFromClient.readLine()).isEmpty()) {
+								if(request.equals("Connection: keep-alive"))
+									socket.setKeepAlive(true);
+								System.out.println(request);
+							}
+							System.out.println("End of GET");
 						}
-						if(requestWords[0].equals("POST") && requestWords[1].equals("/username")) {
-							boolean noBlank = true;
+						if(requestWords[0].equals("POST") && requestWords[1].contains("/username")) {
+							System.out.println("Got post username");
 							String line = "";
 							int contentLength = 0;
-							while(noBlank) {
-								line = inFromClient.readLine();
-								if(line.toLowerCase().contains("Content-length"))
+							while(!(line = inFromClient.readLine()).isEmpty()) {
+								if(line.toLowerCase().contains("content-length"))
 									contentLength = Integer.valueOf(line.split(" ")[1]);
-								if(line.equals(""))
-									noBlank = false;
+								System.out.println(line);
 							}
 							char[] body = new char[contentLength];
+							inFromClient.read(body, 0, contentLength);
 							for(int i = 0; i < body.length; i++) {
 								if(body[i] == '+')
 									body[i] = ' ';
@@ -382,90 +401,91 @@ class ClientServer implements Runnable {
 								}
 							}
 							String stringBody = String.valueOf(body);
-							stringBody = stringBody.substring(6);
-							System.out.println(stringBody);
-							sendPage("/quiz");
-							return stringBody;
+							client.setName(stringBody);
+							sendQuiz();
+							return;
 						}
 					}
 				}
+				else
+					continue;
 			}
 		} catch (IOException e) {
 			active = false;
 			e.printStackTrace();
 		}
-		return null;
+		return;
 	}
 	// Send new quiz info in json format
 	// {"item1":"", "item2":"", "item3":"", "item4":"", "imgPath":"", "vidPath":""}
 	public void sendQuiz() {
 		try {
-			while(true) {
-				String request = inFromClient.readLine();
-				String[] requestWords = request.split(" ");
-				if(requestWords.length == 3) {
-					if(requestWords[0].equals("GET") && requestWords[1].equals("/getQuiz")) {
-						String[] answers = currentQuiz.getAnswers();
-						String imagePath = currentQuiz.getImagePath();
-						String videoPath = currentQuiz.getVideoPath();
-						String json = String.format("\"item1\":\"%s\", \"item2\":\"%s\", \"item3\":\"%s\", \"item4\":\"%s\", \"imgPath\":\"%s\", \"vidPath\":\"%s\"",
-							answers[0], answers[1], answers[2], answers[3], imagePath, videoPath);
+			long currentTime = System.currentTimeMillis();
+			long diff = currentTime - newQuizTime;
+			long waitTime = 13000 - diff;
+			String[] answers = currentQuiz.getAnswers();
 
-						String header = "HTTP/1.0 200 OK\n Server: VineQuiz/1.0 Java/9.0.0\n";
-						SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
-						Date current_time = new Date();
-						String date = formatter.format(current_time).toString();
-						int bodyLength = json.getBytes().length;
-						header = header + date + "\nContent-type: text/json; charset=utf-8\nContent-Length: ";
-						header = header + String.valueOf(bodyLength);
-						String headerBody = header + json;
-						outToClient.writeBytes(headerBody);
-						return;
-					}
-					else if(requestWords[0].equals("GET")) {
-						sendPage(requestWords[1]);
-					}
-				}
-			}
+			String imagePath = currentQuiz.getImagePath();
+			String videoPath = currentQuiz.getVideoPath();
+			String json = String.format("{\"item1\":\"%s\", \"item2\":\"%s\", \"item3\":\"%s\", \"item4\":\"%s\", \"imgPath\":\"%s\", \"vidPath\":\"%s\", \"time\":\"%d\"}",
+				answers[0], answers[1], answers[2], answers[3], imagePath, videoPath, waitTime);
+
+
+			String header = "HTTP/1.0 200 OK\n Server: VineQuiz/1.0 Java/9.0.0\nConnection: Keep-Alive\n";
+			SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
+			Date current_time = new Date();
+			String date = formatter.format(current_time).toString();
+			int bodyLength = json.getBytes().length;
+			header = header + date + "\nContent-Type: application/json; charset=utf-8\nContent-Length: ";
+			header = header + String.valueOf(bodyLength);
+
+			String headerBody = header + "\n\n" + json;
+			outToClient.writeBytes(headerBody);
+			waitAndReceive();
+			return;
 		} catch (IOException e) {
 			active = false;
 			e.printStackTrace();
 		}
 	}
 	// Wait to receive the client's answer to the Quiz. While we wait, check for page requests, ya know.
-	public void waitAndReceive(BufferedReader inFromClient) {
+	public void waitAndReceive() {
 		try {
 			while(true) {
 				String request = inFromClient.readLine();
-				String[] requestWords = request.split(" ");
-				if(requestWords.length == 3) {
-					if(requestWords[0].equals("GET")) {
-						sendPage(requestWords[1]);
-					}
-					if(requestWords[0].equals("POST") && requestWords[1].equals("/submit")) {
-						// Code for parsing the POST request
-						// Should be in x-www-form-urlencoded format
-						boolean noBlank = true;
-						String line = "";
-						int contentLength = 0;
-						while(noBlank) {
-							line = inFromClient.readLine();
-							if(line.toLowerCase().contains("Content-length"))
-								contentLength = Integer.valueOf(line.split(" ")[1]);
-							if(line.equals(""))
-								noBlank = false;
+				System.out.printf("received in waitAndReceive : %s\n", request);
+				if(request != null) {
+					String[] requestWords = request.split(" ");
+
+					if(requestWords.length == 3) {
+						if(requestWords[0].equals("GET")) {
+							sendPage(requestWords[1]);
 						}
-						char[] body = new char[contentLength];
-						String stringBody = String.valueOf(body);
-						int answer = Integer.valueOf(stringBody.split("=")[1]);
-						
-						int score = currentQuiz.calcScore(answer);
-						client.addScore(score);
-						// Update the scores map
-						scores.put(client.getName(), client.getScore());
-						
-						// Send the scores of each client to this client
-						sendScores(currentQuiz.getCorrect(), client.getScore());
+						if(requestWords[0].equals("POST") && requestWords[1].equals("/submit")) {
+							// Code for parsing the POST request
+							// Should be in x-www-form-urlencoded format
+							System.out.println("Received /submit");
+							boolean noBlank = true;
+							String line = "";
+							int contentLength = 0;
+							while(!(line = inFromClient.readLine()).isEmpty()) {
+								if(line.toLowerCase().contains("content-length"))
+									contentLength = Integer.valueOf(line.split(" ")[1]);
+							}
+							char[] body = new char[contentLength];
+							inFromClient.read(body, 0, contentLength);
+							String stringBody = String.valueOf(body);
+							int answer = Integer.valueOf(stringBody);
+
+							int score = currentQuiz.calcScore(answer);
+							client.addScore(score);
+							// Update the scores map
+							scores.put(client.getName(), client.getScore());
+
+							// Send the scores of each client to this client
+							sendScores(currentQuiz.getCorrect(), client.getScore());
+							return;
+						}
 					}
 				}
 			}
@@ -475,29 +495,30 @@ class ClientServer implements Runnable {
 		}
 	}
 	public void sendScores(int correctIdx, int clientScore) {
-		String header = "HTTP/1.0 200 OK\n Server: VineQuiz/1.0 Java/9.0.0\n";
+		String header = "HTTP/1.1 200 OK\nServer: VineQuiz/1.0 Java/9.0.0\nConnection: Keep-Alive\n";
 		SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
 		Date current_time = new Date();
 		String date = formatter.format(current_time).toString();
 
 		int nClients = scores.size();
 		HashMap<String, Integer> sortedScores = sortByValues(scores);
-		String json = String.format("{{\"correctAns\":\"%d\",\"myscore\":\"%d\"}", correctIdx, clientScore);
+		String json = String.format("[{\"correctAns\":\"%d\",\"myscore\":\"%d\"}", correctIdx, clientScore);
 		int i = 0;
 		Set scoreSet = sortedScores.entrySet();
 		Iterator scoreIterator = scoreSet.iterator();
-		while(i < 10) {
+		System.out.println(scoreSet.size());
+		while(i < 10 && i < scoreSet.size()) {
 			Map.Entry<String,Integer> entry = (Map.Entry) scoreIterator.next();
 			String username = entry.getKey();
 			int score = entry.getValue();
 			json = String.format("%s,{\"username\":\"%s\",\"score\":\"%d\"}", json, username, score);
 			i++;
 		}
-		json = json + "}";
+		json = json + "]";
 		int bodyLength = json.getBytes().length;
-		header = header + date + "\nContent-type: text/json; charset=utf-8\nContent-Length: ";
+		header = header + date + "\nContent-Type: application/json; charset=utf-8\nContent-Length: ";
 		header = header + String.valueOf(bodyLength);
-		String headerBody = header + json;
+		String headerBody = header + "\n\n" + json;
 		try {
 			outToClient.writeBytes(headerBody);
 		} catch (IOException e) {
@@ -511,7 +532,7 @@ class ClientServer implements Runnable {
 	public String getPage(String pageName) {
 		String body = getHTML(pageName);
 		if(body.equals("Not Found") || body.equals("Something really went wrong")) {
-			String header = "HTTP/1.0 404 Not Found\n Server: VineQuiz/1.0 Java/9.0.0\n";
+			String header = "HTTP/1.1 404 Not Found\nServer: VineQuiz/1.0 Java/9.0.0\nConnection: Keep-Alive\n";
 			SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
 			Date current_time = new Date();
 			String date = formatter.format(current_time).toString();
@@ -519,7 +540,7 @@ class ClientServer implements Runnable {
 			return header;
 		}
 		else {
-			String header = "HTTP/1.0 200 OK\n Server: VineQuiz/1.0 Java/9.0.0\n";
+			String header = "HTTP/1.1 200 OK\nServer: VineQuiz/1.0 Java/9.0.0\nConnection: Keep-Alive\n";
 			SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
 			Date current_time = new Date();
 			String date = formatter.format(current_time).toString();
@@ -546,18 +567,35 @@ class ClientServer implements Runnable {
 		}
 		catch (IOException ex)
 		{
-			System.out.printf("Error opening file: %s", filename);
-			ex.printStackTrace();
+			System.out.printf("Unable to open file: %s", filename);
+			return "Not Found";
 		}
-		return "Something really went wrong";
+	}
+	private void sendImage(String imgPath) {
+		try {
+			File img = new File(imgPath.substring(1));
+			String header = "HTTP/1.1 200 OK\nServer: VineQuiz/1.0 Java/9.0.0\nConnection: Keep-Alive\n";
+			SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
+			Date current_time = new Date();
+			String date = formatter.format(current_time).toString();
+			long bodyLength = img.length();
+			header = header + date + "\nContent-type: image/jpeg; charset=utf-8\nContent-Length: ";
+			header = header + String.valueOf(bodyLength) + "\n\n";
+			outToClient.writeBytes(header);
+			Files.copy(img.toPath(), outToClient);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	public static int getNewID() {
 		return currentID++;
 	}
 	public static void updateQuiz(Quiz newQuiz){
 		currentQuiz = newQuiz;
+		newQuizTime = System.currentTimeMillis();
 	}
-	private static HashMap sortByValues(Map map) { 
+	private static HashMap sortByValues(Map map) {
 		List list = new LinkedList(map.entrySet());
 		// Defined Custom Comparator here
 		Collections.sort(list, new Comparator() {
@@ -570,7 +608,7 @@ class ClientServer implements Runnable {
 		for (Iterator it = list.iterator(); it.hasNext();) {
 			Map.Entry entry = (Map.Entry) it.next();
 			sortedHashMap.put(entry.getKey(), entry.getValue());
-		} 
+		}
 		return sortedHashMap;
 	}
 }
